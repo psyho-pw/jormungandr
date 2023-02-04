31b427a2776816b997b7e355a6137bdbbd41528b
sayho <fishcreek@naver.com>
2023-02-04 20:21:49 +0900

    fix(slack): fix singleton problem, split token to config

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -2,11 +2,10 @@ import {Module} from '@nestjs/common'
 import {AdminService} from './admin.service'
 import {AdminController} from './admin.controller'
 import {SlackModule} from '../slack/slack.module'
-import {SlackService} from '../slack/slack.service'
 
 @Module({
     imports: [SlackModule],
     controllers: [AdminController],
-    providers: [AdminService, SlackService],
+    providers: [AdminService],
 })
 export class AdminModule {}
--- src/modules/slack/slack.module.ts
+++ src/modules/slack/slack.module.ts
@@ -1,8 +1,10 @@
+import {AppConfigModule} from './../../config/config.module'
 import {Module} from '@nestjs/common'
 import {SlackService} from './slack.service'
 import {SlackController} from './slack.controller'
 
 @Module({
+    imports: [AppConfigModule],
     providers: [SlackService],
     exports: [SlackService],
     controllers: [SlackController],

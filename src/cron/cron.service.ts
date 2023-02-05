596bf5cd18b502191cd9f6ad6d3e4d45f054380f
sayho <fishcreek@naver.com>
2023-02-06 03:26:09 +0900

    fix(cron pattern): fix range

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -14,7 +14,7 @@ export class CronService {
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
     ) {}
 
-    static slackFetchSchedule = '10 * * * * *'
+    static slackFetchSchedule = '0 0 * * * 1-5'
 
     @Cron(CronService.slackFetchSchedule)
     public async fetchSlackInfo() {
